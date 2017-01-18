bbe8550e4e725bd93704034a7bab0358345e7b7e
stefanliydov <stefanlyudov@abv.bg>
2017-01-18 17:20:33 +0200

    First two homework problems.

---
+++ DataTypes/CircleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:f12}",Math.PI *r *r);
+        }
+    }
+}
