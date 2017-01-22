a03664b21ce457a5570642a8fa2102403c99baee
stefanliydov <stefanlyudov@abv.bg>
2017-01-22 02:47:36 +0200

    Some more tasks!

---
+++ DataTypeVarExercises/TheaThePhotographer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pics = long.Parse(Console.ReadLine());
+            var filterTime = long.Parse(Console.ReadLine());
+            var filterFactor = long.Parse(Console.ReadLine());
+            var uploadTime = long.Parse(Console.ReadLine());
+
+            double goodPics = Math.Ceiling(pics * filterFactor / 100.0);
+            var totalTime = (pics * filterTime) + (goodPics * uploadTime);
+
+            
+
+
+            Console.WriteLine(TimeSpan.FromSeconds(totalTime).ToString(@"d\:hh\:mm\:ss"));
+
+
+
+        }
+    }
+}
---
+++ TheaThePhotographer/TheaThePhotographer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pics = int.Parse(Console.ReadLine());
+            var filterTime = int.Parse(Console.ReadLine());
+            var filterFactor = int.Parse(Console.ReadLine());
+            var uploadTime = int.Parse(Console.ReadLine());
+
+            double goodPics = Math.Ceiling(pics * filterFactor / 100.0);
+            var totalTime = (pics * filterTime) + (goodPics * uploadTime);
+
+            int hours = 0;
+            var minutes = totalTime / 60;
+            Console.WriteLine(minutes);
+            if (minutes >= 60)
+                 hours = (int)(minutes / 60);
+
+            Console.WriteLine(hours);
+
+
+
+
+
+        }
+    }
+}
