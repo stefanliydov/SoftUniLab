fe598f200284cfa75d5db1f0a789094e817c8cbd
stefanliydov <stefanlyudov@abv.bg>
2017-01-27 22:28:17 +0200

    Final commitment on Arrays from the lab file.

---
+++ Arrays/ExtractMiddleElement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractMiddleElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] numString = Console.ReadLine().Split(' ');
+
+
+            ReturnMiddleDigits(numString);
+            
+
+        }
+        public static void ReturnMiddleDigits(string[] numString)
+        {
+            if (numString.Length == 1)
+                Console.WriteLine("{ " + $"{numString[0]}" + " }");
+            
+            else if (numString.Length % 2 == 0)
+            {
+                string[] newString = new string[2];
+                var r = 0;
+                for (int i = numString.Length / 2 - 1; i <= numString.Length / 2; i++)
+                {
+                    newString[r] = numString[i];
+                    r++;
+                }
+                string finishedText = string.Join(",", newString);
+                Console.WriteLine("{ " + $"{finishedText}" + " }");
+            }
+            else
+            {
+                string[] newString = new string[3];
+                var r = 0;
+                for (int i = numString.Length / 2 - 1; i <= numString.Length / 2 + 1; i++)
+                {
+                    newString[r] = numString[i];
+                    r++;
+                }
+                string finishedText = string.Join(",", newString);
+                Console.WriteLine("{ " + $"{finishedText}" + " }");
+            }
+        }
+    }
+}
